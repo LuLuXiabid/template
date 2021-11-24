4623292e0a376485bb13267c94f0149d4383f65b
xiao.li <xiao.li@changhong.com>
2021-11-24 16:03:21 +0800

    feat:新增示例

---
+++ springboot-mybatis/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.entity.User;
+import com.example.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+
+	@Autowired
+	private UserService userService;
+	@Test
+	public void contextLoads() {
+		User sel = userService.Sel(1);
+		System.out.println(sel.toString());
+	}
+
+}
